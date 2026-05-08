50cef1ef20b9a6bfb0a1ee75328fb640ba816e17
Wei Yang <wyang007@hotmail.com>
2026-05-07 23:37:26 -0700

    ignore links that require human interaction

--- conf.py
+++ conf.py
@@ -4,3 +4,10 @@ linkcheck_anchors = False
 
 # Remove this later after we fix documenteer
 mermaid_version = "11.9.0"
+
+# Ignore links
+
+linkcheck_ignore = [
+    r'http://grafana.slac.stanford.edu.+/',
+    r'http://docs.redhat.com.+',
+]
