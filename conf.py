c77d03d7290402e4ad7cee8ede3920612897f103
Wei Yang <wyang007@hotmail.com>
2026-05-08 00:06:39 -0700

    disable TLS verification

--- conf.py
+++ conf.py
@@ -9,6 +9,12 @@ mermaid_version = "11.9.0"
 
 linkcheck_ignore = [
     r'https://grafana.slac.stanford.edu.+',
+    r'https://k8s.slac.stanford.edu.+',
     r'https://github.com/slaclab.+',
     r'https://docs.redhat.com.+',
+    r'https://slactraining.skillport.com.+',
 ]
+
+# disable TLS verification
+
+tls_verify = False
